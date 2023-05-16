b0a4e7f282bdc4ace2ef65702ea60cf568bfecb7
bluefly123 <18850075892@163.com>
2023-05-16 16:29:58 +0800

    完成了基本的数据接收功能

---
+++ widget.cpp
@@ -0,0 +1,194 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QTabWidget>
+#include <QDebug>
+
+//用于测试不同tab页面下的操作
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+
+    QChart * chart_realTime = Chart_Init();
+
+    QChartView *chartView = new QChartView();//ui->tab_2
+    QPushButton *button_Test = new QPushButton;
+
+
+    QVBoxLayout *QVBoxLayout_graphPage = new QVBoxLayout;//tab2的布局
+    QHBoxLayout *QHBoxLayout_graphPage_buttonGroup = new QHBoxLayout;//按钮布局
+    QPushButton * button_graphPage_pause = new QPushButton;
+    QPushButton * button_graphPage_clear = new QPushButton;
+    QWidget * widget_graphPage = new QWidget;//tab2的widget
+    QWidget * widget_graphPage_buttonGroup = new QWidget;//tab2的widget
+
+    //    //主要布局为一个tabWidget
+    //    mainLayout->addWidget(ui->tabWidget);
+    //    //设置tab1界面的布局-使用QtDesigner设计
+
+    //    //设置tab2界面的布局
+    button_graphPage_pause->setText("暂停");
+    button_graphPage_clear->setText("清除");
+
+    QHBoxLayout_graphPage_buttonGroup->addWidget(button_graphPage_pause);
+    QHBoxLayout_graphPage_buttonGroup->addWidget(button_graphPage_clear);
+    widget_graphPage_buttonGroup->setLayout(QHBoxLayout_graphPage_buttonGroup);
+    button_Test->setText("测试");
+    chartView->setChart(chart_realTime);//将chart添加到View
+    QVBoxLayout_graphPage->addWidget(chartView);//添加chartView到widget
+    QVBoxLayout_graphPage->addWidget(button_Test);//添加按钮到widget
+    QVBoxLayout_graphPage->addWidget(widget_graphPage_buttonGroup);//添加按钮组到widget
+
+    widget_graphPage->setLayout(QVBoxLayout_graphPage);//将布局添加进widget
+    ui->tabWidget->addTab(widget_graphPage,"图形显示");
+
+
+
+
+}
+
+QChart * Widget::Chart_Init()
+{
+    //初始化QChart的实例
+    QChart *chart;
+    QSplineSeries *lineSeries;
+    chart = new QChart();
+    //初始化QSplineSeries的实例
+    lineSeries = new QSplineSeries();
+    //设置曲线的名称
+    lineSeries->setName("随机数测试曲线");
+    //把曲线添加到QChart的实例chart中
+    chart->addSeries(lineSeries);
+    //声明并初始化X轴、两个Y轴
+    QValueAxis *axisX = new QValueAxis();
+    QValueAxis *axisY = new QValueAxis();
+    //设置坐标轴显示的范围
+    axisX->setMin(0);
+    axisX->setMax(MAX_X);
+    axisY->setMin(0);
+    axisY->setMax(MAX_Y);
+    //设置坐标轴上的格点
+    axisX->setTickCount(10);
+    axisY->setTickCount(10);
+    //设置坐标轴显示的名称
+    QFont font("Microsoft YaHei",8,QFont::Normal);//微软雅黑。字体大小8
+    axisX->setTitleFont(font);
+    axisY->setTitleFont(font);
+    axisX->setTitleText("X-Test");
+    axisY->setTitleText("Y-Test");
+    //设置网格不显示
+    axisY->setGridLineVisible(false);
+    //下方：Qt::AlignBottom，左边：Qt::AlignLeft
+    //右边：Qt::AlignRight，上方：Qt::AlignTop
+    chart->addAxis(axisX, Qt::AlignBottom);
+    chart->addAxis(axisY, Qt::AlignLeft);
+    //把曲线关联到坐标轴
+    lineSeries->attachAxis(axisX);
+    lineSeries->attachAxis(axisY);
+    //把chart显示到窗口上
+    //ui->graphicsView->setChart(chart);
+    // 设置渲染：抗锯齿，如果不设置那么曲线就显得不平滑
+    //ui->graphicsView->setRenderHint(QPainter::Antialiasing);
+    return chart;
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_tab1_clicked()
+{
+
+}
+
+
+void Widget::int2Chars(char str[],long long int a,int &length){
+    length=0;
+    char temp[20];
+    while(a){
+        temp[length]='0'+a%10;
+        a=a/10;
+        length++;
+    }
+    str[length]=0;
+    int i,j;
+    i=0;j=length-1;
+    for(;i<length;i++,j--){
+        str[i]=temp[j];
+    }
+    return;
+}
+
+
+//按下刷新按钮，检查可用串口
+void Widget::on_pushButton_refreshSerial_clicked()
+{
+    qDebug()<<"刷新串口"<<endl;
+    ui->comboBox_serialPort->clear();
+    //通过QSerialPortInfo查找可用串口
+    foreach(const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) //把后面每个项 依次赋值给 info
+    {
+        ui->comboBox_serialPort->addItem(info.portName());
+    }
+    QList<int> baudList=QSerialPortInfo::standardBaudRates();
+    for(int i=0;i<baudList.length();i++){
+        char str[20];
+        int length;
+        int2Chars(str,baudList.at(i),length);
+        //ui->baudSerial->addItem(QString(QByteArray(str,length )));
+    }
+    ui->checkBox_openSerial->setEnabled(true);//使能打开串口
+
+}
+//开启按钮 按下
+void Widget::on_checkBox_openSerial_toggled(bool checked)
+{
+    qDebug()<<checked<<endl;
+    if(true == checked)//判断是否是打开串口
+    {
+        serial->setPortName(ui->comboBox_serialPort->currentText());
+        serial->setBaudRate(QSerialPort::Baud9600);                         //设置波特率
+        serial->setDataBits(QSerialPort::Data8);                            //设置数据位数  默认8位
+        serial->setParity(QSerialPort::NoParity);                           //设置奇偶校验  默认无奇偶
+        serial->setStopBits(QSerialPort::OneStop);                          //设置停止位    默认无停止
+        serial->setFlowControl(QSerialPort::NoFlowControl);                 //设置流控制    默认无
+        if(serial->open(QIODevice::ReadWrite))
+        {
+            qDebug()<<"打开串口成功"<<endl;
+            //QMessageBox::about(NULL, "提示", "打开串口成功");
+            ui->checkBox_openSerial->setText("关闭串口");
+        }
+
+        timer_cycleReceive->start(10);
+        connect(timer_cycleReceive, SIGNAL(timeout()), this, SLOT(on_readSerial()));
+
+    }
+    else
+    {
+        serial->close();
+        timer_cycleReceive->stop();
+    }
+}
+
+void Widget::on_readSerial()
+{
+    static unsigned counter = 0;
+    counter++;
+    if(counter>=4000000000)counter = 0;
+    if(counter%100 == 0)//10ms*100 = 1s
+        qDebug()<<"串口检测"<<counter/100<<"s";
+    QByteArray buffer = serial->readAll();//串口接受数据
+    //buffer = QString::fromLocal8Bit(buffer);//设置成 可以从中文GBK 变成 utf8
+    buffer = buffer.toHex(' ').trimmed().toUpper();/*hex显示*/
+    //ui->textBrowser_receiveShow->clear();// 清空显示
+
+    ui->plainTextEdit_receiveDataShow->insertPlainText(buffer);
+
+}
---
+++ widget.h
@@ -0,0 +1,54 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QtCharts/QChartView>
+#include <QtCharts/QSplineSeries>
+#include <QValueAxis>
+#include <QGraphicsView>
+#include <QChartView>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+
+
+#include <QMessageBox>
+#include <QDebug>
+#include <QTimer>
+
+
+#define MAX_X   50
+#define MAX_Y   70
+QT_CHARTS_USE_NAMESPACE
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    QSerialPort *serial = new QSerialPort;
+    QTimer *timer_cycleReceive = new QTimer;
+
+    Widget(QWidget *parent = nullptr);
+    QChart * Chart_Init();
+
+    void int2Chars(char str[],long long int a,int &length);
+
+    ~Widget();
+
+private slots:
+    void on_pushButton_tab1_clicked();
+
+    void on_pushButton_refreshSerial_clicked();
+
+    void on_checkBox_openSerial_toggled(bool checked);
+
+    void on_readSerial();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
